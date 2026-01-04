110d51c4a896ee4d74bd76e6549ad98639679f1c
Myat Noe <anoe2316@gmail.com>
2026-01-05 00:01:04 +0900

    servo update

--- servoMotor_Project/servoMotor_Project.ino
+++ servoMotor_Project/servoMotor_Project.ino
@@ -1,9 +1,26 @@
+#include <Servo.h>
+Servo myservo;
+
 void setup() {
-  // put your setup code here, to run once:
+  myservo.attach(11);
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int i = 90; i <= 180; i++)
+  {
+      myservo.write(i);
+      delay(20);
+  }
+  
+delay(3000);
 
+for(int i = 180; i >= 90; i--)
+  {
+      myservo.write(i);
+      delay(20);
+  }
+  delay(3000);
 }
