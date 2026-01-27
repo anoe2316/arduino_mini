ebd348f6e3d3d9d15259717b7fb838cc4f70a644
Myat Noe <anoe2316@gmail.com>
2026-01-27 11:15:58 +0900

    ajustment

--- bluetoothSpeaker_Project/bluetoothSpeaker_Project.ino
+++ bluetoothSpeaker_Project/bluetoothSpeaker_Project.ino
@@ -1,7 +1,7 @@
 #include <AltSoftSerial.h>
 #include <DFRobotDFPlayerMini.h>
 
-AltSoftSerial mySerial;   // RX=8, TX=9 (Uno 고정)
+AltSoftSerial mySerial;   
 DFRobotDFPlayerMini player;
 
 void setup() {
@@ -10,10 +10,10 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 
-  delay(1500); // ★ DFPlayer 부팅 대기 (이게 핵심)
+  delay(1500); 
 
   if (!player.begin(mySerial)) {
-    // 실패하면 LED 빠르게 깜빡
+    
     while (1) {
       digitalWrite(13, HIGH); delay(200);
       digitalWrite(13, LOW);  delay(200);
@@ -27,7 +27,7 @@ void loop() {
   if (Serial.available() > 0) {
     char message = Serial.read();
 
-    // 들어오면 LED 한 번 깜빡
+   
     digitalWrite(13, HIGH); delay(80);
     digitalWrite(13, LOW);
 
--- obstacle_alert_system/obstacle_alert_system.ino
+++ obstacle_alert_system/obstacle_alert_system.ino
@@ -1,41 +1,52 @@
+#include <LiquidCrystal.h>
+LiquidCrystal LCD(12,11,5,4,3,2);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(8,INPUT);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(13,OUTPUT);
   Serial.begin(9600);
+  LCD.begin(16,2);
+  LCD.print("Distance:");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(10,LOW);
+  digitalWrite(7,LOW);
   delayMicroseconds(2);
 
-  digitalWrite(10,HIGH);
+  digitalWrite(7,HIGH);
   delayMicroseconds(10);
-  digitalWrite(10,LOW);
+  digitalWrite(7,LOW);
 
   long duration = pulseIn(8,HIGH,25000);
   float dist = duration * 0.034 / 2;
 
-  Serial.print(dist);
-  Serial.println("cm");
+  LCD.setCursor(0,1);
+  LCD.print("      "); // clear area
+  LCD.setCursor(0,1);
+  LCD.print(dist,1);
+  LCD.print(" cm");
   int gap;
 
-  if(dist < 10) gap = 50;
-  else if (dist < 15) gap = 120;
-  else if (dist < 20) gap = 200;
+  if(dist < 10) gap = 50; 
+  else if (dist < 15) gap = 120; 
+  else if (dist < 20) gap = 200; 
   else if (dist < 30) gap = 350;
   else if (dist < 40) gap = 500;
   else
   {
-    noTone(11);
+    noTone(13);
     delay(100);
+    
     return;
   }
-tone(11,262);
+
+tone(13,262);
 delay(80);
-noTone(11);
+noTone(13);
 delay(gap);
+
 }
